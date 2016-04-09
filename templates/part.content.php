cf4e5f06c5757a6f37631bad7941a7e63ff9d2b8
Camila San <smayres@gmail.com>
2016-04-09 18:21:39 +0200

    Mod: UI.

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,24 +1,28 @@
-<div style="padding:30px;">
-    <form id="fileslist-form" method="post" action="/apps/popcorn/list" enctype="multipart/form-data">
-            <h2 style="margin-bottom:20px">Pictures:</h2>
-            <label for="title">Title:</label>
-            <input type="text" name="title" id="title"><br><br>            
-            <label for="file" id="selectfiles" style="padding:10px;border:1px solid #ccc;">Files</label>
-            <input type="file" name="file" id="file" style="display:none"><br><br>
-            <label for="title">Theme:</label>
-            <select name="theme" id="theme">
-                <option value="0">Black & White</option>
-                <option value="1">The Happy One</option>
-            </select>
-            <br><br>                        
-            <br>
-            <button name="submit" id="submit" style="margin-top:30px;border:1px #ccc solid;padding:10px">Create video!</button><br>
-    </form>
-    <div id="files">
-        <ul></ul>
-    </div>
+<div class="popcorn">
     <div id="error"></div>
-    <div id="video" style="padding:30px;display:none;">
+    <form id="fileslist-form" method="post" action="/apps/popcorn/list" enctype="multipart/form-data">
+            <div class="app-logo"><img src="/apps/popcornapp/img/app-logo.gif"></div>
+            <div class="form-container">
+                <label for="title">Title:</label>
+                <input type="text" name="title" id="title">            
+                <label for="title">Theme:</label>
+                <select name="theme" id="theme">
+                    <option value="0">Black & White</option>
+                    <option value="1">The Happy One</option>
+                </select>   
+                <br>
+                <div class="files-container" id="selectfiles">
+                    <label for="file">Select Files</label>
+                    <input type="file" name="file" id="file">
+                    <div id="files">
+                        <ul></ul>
+                    </div> 
+                </div>                
+                <button name="submit" id="submit">Create video</button>
+            </div>   
+    </form>    
+    <div id="progress"><img src="/apps/popcornapp/img/ajax-loader.gif"></div>
+    <div id="video">
         <video width="600" height="400" controls><source type="video/mp4">Your browser does not support the video tag.</video>    
     </div>
 </div>
